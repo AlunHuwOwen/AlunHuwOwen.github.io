7c4bfe29342e48610a43763f43cbbb4646ae78e8
Alun Owen <alunhowen@gmail.com>
2021-09-24 15:19:28 +0100

    Stage 1 of Slideshow and Google Analytics

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function doIt() {
     $('#occupation').html(changer)
     //occupation.innerHTML = changer;
     //$("#occupation").fadeIn(2500);
-    console.log(occupation);
+    //console.log(occupation);
 }
 
 function cycle() {
@@ -29,3 +29,33 @@ function cycle() {
 }
 
 cycle();
+
+/* 
+Now look at slideshow animations...
+*/
+
+var slideInd = 1;
+showSlides(slideInd);
+
+/* Prev/Next control mechanism */
+
+function plusSlides(n) {
+    showSlides(slideInd += n)
+}
+
+function thisSlide(n) {
+    showSlides(slideInd = n)
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide")
+    if (n > slides.length) {slideInd = 1}
+    if (n < 1) {slideInd = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideInd-1].style.display = "block";
+}
+
+var img1 = getElementById("img1")
